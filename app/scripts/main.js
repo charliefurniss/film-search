088468716951251e5c4f70d7b73580f28ee2d3d6
Charlie Furniss <charliefurniss96@gmail.com>
2016-08-03 14:52:14 +0100

    sort buttons colours toggling on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     
     var sortType = 'TITLE';
+    var activeId = 'title';
+    var passiveId = 'year';
     var datas = [];
 
     $('#search-input').on('input', function(e) {
@@ -11,21 +13,34 @@ $(document).ready(function () {
     });
 
     $('#title-input, #date-input').on('click', function(e) {
-        console.log(e.currentTarget.value);
+        
         switch (e.currentTarget.value) {
             case 'TITLE':
                 sortType = 'TITLE';
+                activeId = 'title';
+                passiveId = 'date';
                 break;
             case 'RELEASE YEAR':
                 sortType = 'RELEASE YEAR';
+                activeId = 'date';
+                passiveId = 'title';
                 break;
             default:
                 sortType = 'TITLE';
+                activeId = 'title';
+                passiveId = 'date';
                 break;
         }
 
+        toggleColour(e);
+
         render();
     });
+
+    function toggleColour(e) {
+        $('#' + activeId + '-input').addClass('sort-active').removeClass('sort-button');
+        $('#' + passiveId + '-input').removeClass('sort-active').addClass('sort-button');
+    }
 
     function sortDatas() {
         var sortProp = '';
@@ -62,6 +77,11 @@ $(document).ready(function () {
 
         $.each(datas, function(index, data) {
             // console.log(data);
+
+            if(data.Type == 'movie') {
+                data.Type = 'film';
+            }
+
             $('.film-list').append('<li class="film-listing col-md-4"></li>')
             $('.film-list').find('li').eq(index).append('<div class="film-container"><div class="film-inner row">'+ ' <div class="col-md-3"><img class="film-poster" alt="No image" src="' + data.Poster + '"/></div> <div class="film-info col-md-9"><div class="film-type">• ' + data.Type + '</div><div class="film-title">' + data.Title + '</div><div class="film-year">' + data.Year + '</div></div></div></div>');
         });
